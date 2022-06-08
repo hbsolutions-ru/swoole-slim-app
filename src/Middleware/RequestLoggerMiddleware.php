2f4b7bd29df839217f698e205a70c21ea5f28c76
HBS Development Team <dev.hbsolutions@gmail.com>
2022-06-08 12:14:11 +0700

    [FEATURE] Add implementation of the Request Logger Middleware.

---
+++ src/Middleware/RequestLoggerMiddleware.php
@@ -0,0 +1,43 @@
+<?php declare(strict_types=1);
+
+namespace HBS\SwooleSlimApp\Middleware;
+
+use Psr\Http\Message\{
+    ResponseInterface as Response,
+    ServerRequestInterface as Request,
+};
+use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
+use HBS\Helpers\DateTimeHelper;
+
+final class RequestLoggerMiddleware implements RequestLoggerMiddlewareInterface
+{
+    private const DEFAULT_DATE_TIME_FORMAT = 'Y-m-d\\TH:i:s\\.uP';
+
+    /**
+     * @var string
+     */
+    private $dateTimeFormat;
+
+    public function __construct(?string $dateTimeFormat = null)
+    {
+        $this->dateTimeFormat = $dateTimeFormat ?? self::DEFAULT_DATE_TIME_FORMAT;
+    }
+
+    public function process(Request $request, RequestHandler $handler): Response
+    {
+        $now = DateTimeHelper::now();
+
+        $response = $handler->handle($request);
+
+        printf(
+            "[%s] \"%s %s HTTP/%s\" %d" . PHP_EOL,
+            $now->format($this->dateTimeFormat),
+            strtoupper($request->getMethod()),
+            $request->getUri()->getPath(),
+            $request->getProtocolVersion(),
+            $response->getStatusCode()
+        );
+
+        return $response;
+    }
+}
